dad9b1e9fb7d0aef1afb59732234abc3f5c53d08
DONGHWAN <midnightinmyjourney@gmail.com>
2022-07-07 23:12:16 +0900

    Spring Configuration And Byte Code

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,21 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //    class hello.core.AppConfig$$EnhancerBySpringCGLIB$$
+        //내가만든 class아님 -> Spring이 임의의 클래스를 만듦
+        //기능.. 이미 생성된 Bean은 호출 없으면 생성!
+        //AppConfig의 자식타입이라서 AppConfig타입으로 조회 가능!!
+        
+        //@Configuration 을 AppConfig에서 빼면?
+        //Bean은 다 등록되긴 함 But 기존 생성된 Bean을 다시 불러오진 못함 => Singleton 깨짐 (순수 java 코드 실행..)
+        
+        //@Autowired로 해결 가능하긴함.. 직접연결하여 Bean 재사용
+    }
 }
